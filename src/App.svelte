e62fb57fee8be4d7582b5e02d951f9547d85f44b
youxiang-git <chaiyouxiang@gmail.com>
2026-01-31 19:07:38 +0800

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import Counter from './lib/Counter.svelte'
+
+  const id = "What's your name: "
+  const defaultUser = "Current User"
+  let username = $state("");
+</script>
+
+<main>
+  <h1>Welcome to {username || defaultUser}'s Coffee Diary</h1>
+
+  <div class="card">
+    <Counter />
+  </div>
+
+  <label for={id}>{id}</label>
+  <input {id} bind:value={username}/>
+
+</main>
+
+<style>
+</style>

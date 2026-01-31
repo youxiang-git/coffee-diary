dbb57950bb07c36ab69cc9b465649676cbd0816c
youxiang-git <chaiyouxiang@gmail.com>
2026-01-31 19:59:02 +0800

    update readme to placeholder, added tailwindcss

--- src/App.svelte
+++ src/App.svelte
@@ -1,22 +1,20 @@
 <script lang="ts">
-  import Counter from './lib/Counter.svelte'
+	import './app.css';
+	import Counter from './lib/Counter.svelte';
 
-  const id = "What's your name: "
-  const defaultUser = "Current User"
-  let username = $state("");
+	const id = "What's your name: ";
+	const defaultUser = "Current User";
+	let username = $state("");
 </script>
 
 <main>
-  <h1>Welcome to {username || defaultUser}'s Coffee Diary</h1>
+	<h1>
+		Welcome to {username || defaultUser}'s Coffee Diary
+	</h1>
 
-  <div class="card">
-    <Counter />
-  </div>
-
-  <label for={id}>{id}</label>
-  <input {id} bind:value={username}/>
-
+	<div class="card"><Counter /></div>
+	<label for={id}>{id}</label>
+	<input id={id} bind:value={username} />
 </main>
 
-<style>
-</style>
+<style></style>
